727f5220e0dffd4d6b17a53dd1d0692a46dc729e
Parallel <llelparallel@gmail.com>
2022-04-26 23:41:45 +0700

    🚀 Deploy to Github Pages

---
+++ _next/static/chunks/1208.1cbfd5c55ac66e32.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[1208],{36563:function(){},89214:function(){},69386:function(){},31616:function(){},29120:function(){},46586:function(){},6567:function(){}}]);
